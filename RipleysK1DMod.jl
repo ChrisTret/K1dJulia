12dc9ef57edaf4a7f4a3629a9473ee2131705b44
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-06-09 11:41:48 -0500

    Include pre_process and all comparisons fucntions

--- RipleysK1DMod.jl
+++ RipleysK1DMod.jl
@@ -1,13 +1,14 @@
 module RipleysK1D
 
-using Plots
+using CSV, DataFrames, Plots
 
-include("k1d_univ_Mod.jl")
+include("data_wranglingMod.jl")
+include("k1d_univMod.jl")
 include("k1d_bivariateMod.jl")
-include("k1d_speed_tests_Mod.jl")
+include("k1d_speed_testsMod.jl")
 include("k_plotsMod.jl")
-using .K1dUniv, .K1dBiv, .K1dSpeedTests, .K1dPlots
+using .DataWrangling, .K1dUniv, .K1dBiv, .K1dSpeedTests, .K1dPlots
 
-export k1d_univ, k1d_biv, speed_comparison_univ, k_plot
+export process_genome_data, k1d_univ, k1d_biv, k1d_biv_all_comparisons, speed_comparison_univ, k_plot
 
 end # end of module
