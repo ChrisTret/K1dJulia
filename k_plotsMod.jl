6b65b01bb3821ce49c4dd23d82e643f7d33abf1f
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-06-10 13:13:01 -0500

    Alter to dictionary based k_plot

--- k_plotsMod.jl
+++ k_plotsMod.jl
@@ -10,19 +10,23 @@ using Plots
 Plots both the observed K̂ and the theoretical K under CSR against T
 
 # Arguments
-`K̂::Vector{Float64}`: Vector of observed K̂ values from k1d_univ
+`K_dict::Dict{Tuple{String, String}, Vector{Float64}}`: Dictionary containing the data vectors
+`keys::Tuple{String, String}`: Tuple of keys to access the data
 `T::Vector{Int64}`: Vector of t values searched over
 
 """
-function k_plot(K̂::Vector{Float64}, T::Vector{Int64})
-   
+function k_plot(K_dict::Dict{Tuple{String, String}, Vector{Float64}}, keys::Tuple{String, String}, T::Vector{Int64})
+    K̂ = K_dict[keys]
+    key1, key2 = keys
+    
     # E(K̂) under CSR
     theoretical = 2 * T
-
-    plot(T, theoretical, color = :blue, label = "Theoretical", title = "Observed vs Theoretical K Values")
+    
+    # Generate title based on keys
+    title = "Observed vs Theoretical K Values: $key1 - $key2"
+    
+    plot(T, theoretical, color = :blue, label = "Theoretical", title = title)
     plot!(T, K̂, color = :red, label = "Observed")
-
 end
 
 end # end of module
-
