3b145f90dfbb727367ad03dd570f5fdaaf4d1010
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-06-18 14:37:03 -0500

    Add SigMeasures and K1dConfInt modules

--- RipleysK1DMod.jl
+++ RipleysK1DMod.jl
@@ -1,14 +1,18 @@
 module RipleysK1D
 
-using CSV, DataFrames, Plots
+using Revise, CSV, DataFrames, Plots
 
 include("data_wranglingMod.jl")
 include("k1d_functionsMod.jl")
 include("k1d_speed_testsMod.jl")
 include("k_plotsMod.jl")
-using .DataWrangling, .K1dFun, .K1dSpeedTests, .K1dPlots
+include("k1d_conf_intMod.jl")
+include("significance_measuresMod.jl")
 
-export process_genome_data, k1d_univ, k1d_biv, k1d_all_comparisons, speed_comparison_univ, k_plot
+using .DataWrangling, .K1dFun, .K1dSpeedTests, .K1dPlots, .K1dConfInt, .SigMeasures
+
+export process_genome_data, k1d_univ, k1d_biv, k1d_all_comparisons, speed_comparison_univ, k_plot, plot_top_n
+export bootstrap_variance_k1d, max_percent_increase
 
 end # end of module
 
