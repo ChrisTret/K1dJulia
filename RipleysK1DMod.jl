24724255c0fb32ab72bfa855bcbb0fe511e4e491
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-06-20 12:13:34 -0500

    Add process and filter function

--- RipleysK1DMod.jl
+++ RipleysK1DMod.jl
@@ -1,6 +1,6 @@
 module RipleysK1D
 
-using Revise, CSV, DataFrames, Plots
+using Revise, CSV, DataFrames, Statistics, Plots
 
 include("data_wranglingMod.jl")
 include("k1d_functionsMod.jl")
@@ -11,8 +11,12 @@ include("significance_measuresMod.jl")
 
 using .DataWrangling, .K1dFun, .K1dSpeedTests, .K1dPlots, .K1dConfInt, .SigMeasures
 
-export process_genome_data, k1d_univ, k1d_biv, k1d_all_comparisons, speed_comparison_univ, k_plot, plot_top_n
-export bootstrap_variance_k1d, max_percent_increase
+export process_genome_data, process_filtered_genome_data 
+export k1d_univ, k1d_biv, k1d_all_comparisons 
+export speed_comparison_univ 
+export k_plot, plot_top_n
+export bootstrap_variance_k1d 
+export max_percent_increase
 
 end # end of module
 
--- data_wranglingMod.jl
+++ data_wranglingMod.jl
@@ -1,9 +1,10 @@
 module DataWrangling
 
-export process_genome_data
+export process_genome_data, process_filtered_genome_data
 
 using CSV
 using DataFrames
+using Statistics
 
 """
     process_genome_data(file_path::String) -> Dict{String, Vector{Float64}}
@@ -43,5 +44,40 @@ function process_genome_data(file_path::String, cluster_group::String, start_col
     
     return geno_dict
 end
+
+
+"""
+    process_filtered_genome_data(file_path::String, cluster_group::String, start_colname::String, end_colname::String, percentile::Float64) -> Dict{String, Vector{Int64}}
+
+Read a CSV file containing genomic data, filter entries based on the length percentile specified by the user, and compute the genomic center for each entry, organized by the specified `cluster_group`.
+
+# Arguments
+- `file_path::String`: The path to the CSV file containing the genomic data.
+- `cluster_group::String`: The name of the column by which the data should be grouped (e.g., 'repFamily').
+- `start_colname::String`: The name of the column indicating the start of an element.
+- `end_colname::String`: The name of the column indicating the end of an element.
+- `percentile::Float64`: The percentile value used to filter data based on the length of elements. The function filters out the shortest elements, keeping only those in the top `1 - percentile` fraction.
+
+# Returns
+- `Dict{String, Vector{Int64}}`: A dictionary where each key corresponds to a unique value from the `cluster_group` column, and each value is a vector of integers representing the genomic center of the filtered entries for that group.
+"""
+function process_filtered_genome_data(file_path::String, cluster_group::String, start_colname::String, end_colname::String, percentile::Float64)::Dict{String, Vector{Int64}}
+    data = CSV.read(file_path, DataFrame; header = 1)
+    # Calculate lengths and filter based on the specified percentile
+    data[!, "Length"] = data[:, end_colname] - data[:, start_colname]
+    cutoff = quantile(data[!, "Length"], 1 - percentile)
+    filtered_data = filter(row -> row["Length"] >= cutoff, data)
+    # Calculate centers of the filtered data
+    filtered_data[!, "Center"] = round.(Int, (filtered_data[:, start_colname] .+ filtered_data[:, end_colname]) ./ 2)
+    filtered_dict = Dict{String, Vector{Int64}}()
+    grouped_data = groupby(filtered_data, cluster_group)
+    for g in grouped_data
+        family = g[1, cluster_group]
+        geno_centers = g[:, "Center"]
+        filtered_dict[family] = geno_centers
+    end
+    return filtered_dict
+end
+
 
 end # end of module
