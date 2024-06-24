7a9914eb6029bf7edd390b84886ae1536d856670
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-06-24 14:41:45 -0500

    Update main mod

--- RipleysK1DMod.jl
+++ RipleysK1DMod.jl
@@ -1,6 +1,6 @@
 module RipleysK1D
 
-using Revise, CSV, DataFrames, Statistics, Plots
+using CSV, DataFrames, Statistics, Plots
 
 include("data_wranglingMod.jl")
 include("k1d_functionsMod.jl")
@@ -11,10 +11,10 @@ include("significance_measuresMod.jl")
 
 using .DataWrangling, .K1dFun, .K1dSpeedTests, .K1dPlots, .K1dConfInt, .SigMeasures
 
-export process_genome_data, process_filtered_genome_data 
+export process_genome_data, merge_dictionaries
 export k1d_univ, k1d_biv, k1d_all_comparisons 
 export speed_comparison_univ 
-export k_plot, plot_top_n
+export k_plot, plot_top_n, plot_by_key, plot_pair
 export bootstrap_variance_k1d 
 export max_percent_increase
 
