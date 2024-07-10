8bdc13758fa0d95783e73c4285a7a3610203c567
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-07-10 15:07:19 -0500

    Finish univariate simulation

--- k1d_monte_carlo.jl
+++ k1d_monte_carlo.jl
@@ -1,6 +1,8 @@
 module K1dMonteCarlo
 
-using Distributions
+include("k1d_functionsMod.jl")
+
+using .K1dFun, Distributions
 
 export monte_carlo_sim
 
@@ -9,14 +11,14 @@ function monte_carlo_sim(data::Dict{String,Vector{Int64}}, keys::Tuple{String, S
     key2 = keys[2]
     X = data[key1]
     Y = data[key2]
-    mc_results = zeros(T,m)
+    mc_results = zeros(length(T),m)
 
     if key1 == key2
         N = length(X)
         lambda_hat = N / (X[N] - X[1] + 1)
         mc_results[:,1] = k1d_univ(X,T)
         for i in 2:m
-            mc_sample = cumsum(rand(Exponential(lambda_hat), N))
+            mc_sample = round.(Int, cumsum(rand(Exponential(1/lambda_hat), N)))
             mc_results[:,i] = k1d_univ(mc_sample, T)
         end
     else
@@ -25,9 +27,9 @@ function monte_carlo_sim(data::Dict{String,Vector{Int64}}, keys::Tuple{String, S
         lambda_hat_x = Nx / (X[Nx] - X[1] + 1)
         lambda_hat_y = Ny / (Y[Ny] - Y[1] + 1)
         mc_results[:,1] = k1d_biv(X,Y,T)
-        for i in 2:m
-            mc_sample_x = cumsum(rand(Exponential(lambda_hat_x), Nx))
-            mc_sample_y = cumsum(rand(Exponential(lambda_hat_y), Ny))
+        for i in 2:3
+            mc_sample_x = round.(Int, cumsum(rand(Exponential(1/lambda_hat_x), Nx)))
+            mc_sample_y = round.(Int, cumsum(rand(Exponential(1/lambda_hat_y), Ny)))
             mc_results[:,i] = k1d_biv(mc_sample_x, mc_sample_y, T)
         end
     end
