b7864eba7f07ddaa78d76ca0e8e6c14c4f75a60c
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-07-11 14:52:56 -0500

    Add bias checking codes

--- test_codes.jl
+++ test_codes.jl
@@ -1,6 +1,5 @@
 include("k1d_functionsMod.jl")
-using K1dUniv
-using K1dBiv
+using .K1dFun
 
 # univariate
 # hand-calculated 4/3, 8/3. 32/3
@@ -14,3 +13,25 @@ test2 = [4, 5, 10, 19, 23, 24, 25, 30]
 # K_12 = K_21 = 2.8125 6.5625 15.0000
 println(k1d_biv(test1, test2, [3, 4, 10]))
 println(k1d_biv(test2, test1, [3, 4, 10]))
+
+
+# code for checking bias
+my_sim = monte_carlo_sim(full_dict, key_pair, T, 101) 
+monte_carlo_test(my_sim)
+flag = zeros(99)
+binomial_results = zeros(100)
+for i in  1:100
+    my_sim = monte_carlo_sim(full_dict, key_pair, T, 101) 
+    for j in 2:100
+        stuff = my_sim[:,j]
+        vec = stuff - 2T
+        if length(vec[vec .> 0]) >= 50
+            flag[j-1] = 1
+        else
+            flag[j-1] = -1
+        end
+    end
+    binomial_results[i] = length(flag[flag .> 0])
+end
+histogram(binomial_results)
+title!("Histogram of Positively Biased Estimators")
