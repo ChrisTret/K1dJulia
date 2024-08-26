4815cc5cbbe948c575618e4e7f30f6bf86fc819a
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-08-26 12:13:59 -0500

    Include K1dPrepreocess

--- RipleysK1DMod.jl
+++ RipleysK1DMod.jl
@@ -3,6 +3,7 @@ module RipleysK1D
 using CSV, DataFrames, Statistics, Plots
 
 include("data_wranglingMod.jl")
+include("k1d_preprocessMod.jl")
 include("k1d_functionsMod.jl")
 include("k1d_speed_testsMod.jl")
 include("k1d_plotsMod.jl")
@@ -11,9 +12,10 @@ include("significance_measuresMod.jl")
 include("k1d_monte_carlo.jl")
 include("k1d_ztestMod.jl")
 
-using .DataWrangling, .K1dFun, .K1dSpeedTests, .K1dPlots, .K1dConfInt, .SigMeasures, .K1dMonteCarlo, .K1dZTests
+using .DataWrangling, .K1dPreprocess, .K1dFun, .K1dSpeedTests, .K1dPlots, .K1dConfInt, .SigMeasures, .K1dMonteCarlo, .K1dZTests
 
 export process_genome_data, merge_dictionaries, process_genome_data_by_chromosome, process_genome_data_by_region, process_genome_data_by_chromosome_by_region
+export display_counts, filter_data_by_occurrences!
 export k1d_univ, k1d_biv, k1d_all_comparisons, k1d_all_comparisons_indiv_chrom,  k1d_mean_across_chromosomes, k1d_mean_across_regions
 export speed_comparison_univ 
 export k_plot, l_plot, z_plot, plot_top_n, plot_by_key, plot_pair
