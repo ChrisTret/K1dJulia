28d0fc3bd6970f505de33c7abf8981e508111546
Chris Trettel <112290430+ChrisTret@users.noreply.github.com>
2024-11-22 11:49:43 -0600

    Add Region Variance

--- k1d_conf_intMod.jl
+++ k1d_conf_intMod.jl
@@ -116,6 +116,51 @@ function approx_k1d_mean_variance_chromosome(results::Dict{Tuple{String, String,
     return k_mean_var
 end
 
+"""
+    approx_k1d_mean_variance_region(results::Dict{Tuple{String, String, String, Int}, Vector{Float64}}, 
+                                    k_means::Dict{Tuple{String, String}, Vector{Float64}}, 
+                                    key_pair::Tuple{String, String}) -> Dict{Tuple{String, String}, Vector{Float64}}
+
+Calculates the approximate variance of the Kmean for a specific key pair across different regions based on the provided results.
+
+# Arguments
+- `results::Dict{Tuple{String, String, String, Int}, Vector{Float64}}`: 
+  A dictionary where each key is a tuple containing two element names, a chromosome identifier, and a region identifier, and each value is a vector of K statistics for that region.
+- `k_means::Dict{Tuple{String, String}, Vector{Float64}}`: 
+  A dictionary where each key is a tuple of two element names, and each value is a vector representing the mean values of K across regions.
+- `key_pair::Tuple{String, String}`: 
+  A tuple containing two element names to specify which pair's variance to compute.
+
+# Returns
+- `Dict{Tuple{String, String}, Vector{Float64}}`: 
+  A dictionary where the key is the provided `key_pair`, and the value is a vector of variances for the K mean across regions.
+"""
+function approx_k1d_mean_variance_region(results::Dict{Tuple{String, String, String, Int}, Vector{Float64}}, k_means::Dict{Tuple{String, String}, Vector{Float64}}, key_pair::Tuple{String, String})
+    len_T = length(k_means[key_pair])
+    k_mean = k_means[key_pair]
+    k_mean_var = Dict{Tuple{String, String}, Vector{Float64}}(key_pair => zeros(len_T))
+
+    # Filter keys matching the key_pair across all regions
+    filtered_keys = filter(key -> key[1] == key_pair[1] && key[2] == key_pair[2], keys(results))
+    R = length(filtered_keys)
+
+    if R > 1
+        for key in filtered_keys
+            diff = results[key] .- k_mean
+            k_mean_var[key_pair] .+= diff .* diff
+        end
+
+        scale_factor = 1 / (R * (R - 1))
+        k_mean_var[key_pair] .*= scale_factor
+    else
+        k_mean_var[key_pair] .= NaN
+        println("Not enough data points to calculate variance for key pair $(key_pair).")
+    end
+
+    return k_mean_var
+end
+
+
 
 """
     approx_k1d_mean_variance_all_comparisons(results::Dict{Tuple{String, String, String}, Vector{Float64}}, 
@@ -148,5 +193,28 @@ function approx_k1d_mean_variance_all_comparisons(results::Dict{Tuple{String, St
 
     return all_variances
 end
+
+
+function approx_k1d_mean_variance_all_comparisons(
+    results::Dict{Tuple{String, String, String, Int}, Vector{Float64}},
+    k_means::Dict{Tuple{String, String}, Vector{Float64}}
+)::Dict{Tuple{String, String}, Vector{Float64}}
+    # Initialize the dictionary to store variances
+    all_variances = Dict{Tuple{String, String}, Vector{Float64}}()
+
+    # Iterate over all unique key pairs from the results dictionary
+    for key in keys(results)
+        key_pair = (key[1], key[2])
+        
+        # Check if the variance for this key pair has already been calculated
+        if !haskey(all_variances, key_pair)
+            variance = approx_k1d_mean_variance_region(results, k_means, key_pair)
+            all_variances[key_pair] = variance[key_pair]
+        end
+    end
+
+    return all_variances
+end
+
 
 end # end of module
